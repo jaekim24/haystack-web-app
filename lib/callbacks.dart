3f70e142e841ceaf5898f97d7226af407332bcea
jae <jaekim242@gmail.com>
2026-01-06 23:16:50 -0500

    everythjng works pushing before making bug UI chnage

--- lib/callbacks.dart
+++
@@ -1,6 +0,0 @@
-
-import 'accessory/accessory_model.dart';
-
-typedef LoadLocationUpdatesCallback = Future<void> Function(Accessory? data);
-typedef SaveOrderUpdatesCallback = Future<void> Function(
-    List<Accessory> accessories);
